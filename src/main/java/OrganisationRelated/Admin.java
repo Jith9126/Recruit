236d1a3462d88a5a0dce977668c5261f9ad54f05
ragavikannan123 <160319873+ragavikannan123@users.noreply.github.com>
2024-02-18 20:13:36 +0530

    Update Admin.java

--- src/main/java/OrganisationRelated/Admin.java
+++ src/main/java/OrganisationRelated/Admin.java
@@ -18,14 +18,5 @@ public class Admin extends User{
 	public void setOrganisation(Organisation organisation) {
 		this.organisation = organisation;
 	}
-
-	public void addPanelist(String name,String email,Gender gender, String position, int Department_Id, int Org_Id) {
-		
-		DBConnection db = DBConnection.getDB();
-		Connection connection = db.getConnection();
-
-		String query = "insert into PaneList (Name, Email, Gender, Department_Id, Org_Id, Position) values (?, ?, ?, ?, ?, ?)";
-		PrePa
-		
-	}
+	
 }

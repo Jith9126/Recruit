2d1a8c0b5ee7d531ae6338c56691b9be7681f06a
ragavikannan123 <160319873+ragavikannan123@users.noreply.github.com>
2024-02-18 11:11:22 +0530

    Update DBConnection.java

--- src/main/java/OrganisationRelated/DBConnection.java
+++ src/main/java/OrganisationRelated/DBConnection.java
@@ -1,2 +1,31 @@
 package OrganisationRelated;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	static DBConnection db;
+	Connection connection = null;
+
+    public DBConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "";
+           
+            this.connection = DriverManager.getConnection(url, "", "");
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+
+    public static DBConnection getDB() {
+        if (db == null) {
+        	db = new DBConnection();
+        }
+        return db;
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+}
